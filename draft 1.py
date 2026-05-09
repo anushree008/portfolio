7ffc16d0124e2db996b9fc2f15f9288f9e0e75e1
Anushree <96528824+anushree008@users.noreply.github.com>
2026-05-09 15:36:16 +0200

    Update draft 1.py

--- draft 1.py
+++ draft 1.py
@@ -8,3 +8,14 @@ print(f"Current time:\n{current_time_Human_format}")
 # 1500 sec in 25 min
 min,sec = divmod(1500,60)
 print(f"{min:02d}:{sec:02d}")
+
+#trail 3 changing countdown 
+#total seconds
+time_left = 1500 
+for i in range(time_left):
+    #recalculate min/sec from the total time left
+    mins, secs = divmod(time_left, 60)
+    print(f"{mins:02d}:{secs:02d}")
+    #wait and then subtract from the total
+    time.sleep(1)
+    time_left -= 1 
