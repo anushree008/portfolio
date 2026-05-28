4d49e5a6272bd8cb157174545c8d5bc37640adce
Anushree <96528824+anushree008@users.noreply.github.com>
2026-05-28 21:50:46 +0200

    Update budgeting trial 2.py

--- budgeting trial 2.py
+++ budgeting trial 2.py
@@ -68,6 +68,7 @@ while True:
             view_choice = int(input("What do you wish to view?\n1. History\n2. Calender\n3. Back to main menu\n\nChoice: "))
             match view_choice:
                 case 1:
+                    history_choice = int(input("What do you wish to view?\n1. Montly expenses\n2. Income summary\n3. Expense summary\n4. Budget list\n5. Back to main menu\n\nChoice: "))
                     total_income = 0
                     total_exppense = 0
                     total_budget = 0
@@ -77,12 +78,24 @@ while True:
                         total_expense += i["Amount"]
                     for i in Budget:
                         total_budget += i["Amount"]
-                    print(f"Your total income was {total_income}.\nYour total expense was {total_expense}.")
-                    print(f"Income summary:\n{Income}\n\nExpense summary:\n{Expense}\n\n")
-                    if total_expense > total_budget:
-                        print(f"Budget was exceeded by {total_expense - total_budget}.")
-                    else:
-                        print(f"You managed to stay within the budget by {total_budget - total_expense}.")
+                    match history_choice:
+                        case 1:
+                            print(f"Your total income was {total_income}.\nYour total expense was {total_expense}.")
+                            if total_expense > total_budget:
+                                print(f"Budget was exceeded by {total_expense - total_budget}.")
+                            else:
+                                print(f"You managed to stay within the budget by {total_budget - total_expense}.")
+                        case 2:
+                            print(f"Income summary:\n{Income}\n")
+                        case 3:
+                            print(f"Expense summary:\n{Expense}\n\n")
+                        case 4:
+                            for i, budget in enumerate(Budget):
+                                print(f"{i}: {budget}")
+                        case 5:
+                            break
+                        case _:
+                            print("Error: Please enter a valid choice!")
                 case 2:
                     print("Onthe way!")
                 case 3:
