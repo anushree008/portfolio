be99f8cc36d3bacb985d5c047ca3bbb963246db1
Anushree <96528824+anushree008@users.noreply.github.com>
2026-06-03 09:34:00 +0200

    Update calender trail.py

--- calender trail.py
+++ calender trail.py
@@ -1,4 +1,14 @@
+#importing all nessasary libraries
 import datetime
 import calendar
 import customtkinter
 import json
+
+#making the window
+Calendar = customtkinter.CTk()
+Calendar.title("Calender")
+Calendar.geometry("400x300")
+
+
+button = customtkinter.CTkButton(Calendar, text="Click me")
+button.pack()
